46a6d702b768a81bf91700778007ee3919702290
TheBinKingVII <bintoronob@gmail.com>
2025-04-02 12:34:51 +0700

    update bottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quran_app/globals.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +9,161 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home Screen')),
+      backgroundColor: background,
+      appBar: _appBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Assalamualaikum',
+              style: GoogleFonts.poppins(
+                  fontSize: 18, fontWeight: FontWeight.w500, color: text),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Bintoro Linkebin',
+              style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            _lastRead()
+          ],
+        ),
+      ),
+      bottomNavigationBar: _bottomNavigationBar(),
+    );
+  }
+
+  //Sampai sini
+  Stack _lastRead() {
+    return Stack(
+      children: [
+        Container(
+          height: 131,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [
+                    0,
+                    0.6,
+                    1
+                  ],
+                  colors: [
+                    Color(0xFFDF98FA),
+                    Color(0xFFB070FD),
+                    Color(0xFF9055FF)
+                  ])),
+        ),
+        Positioned(
+            bottom: 0,
+            right: 0,
+            child: SvgPicture.asset('assets/svgs/quran.svg')),
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  SvgPicture.asset('assets/svgs/book.svg'),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    'Last Read',
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontWeight: FontWeight.w500),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Al-Fatihah',
+                style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                'Ayat No: 1',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  BottomNavigationBar _bottomNavigationBar() => BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          _bottomBarItem(icon: 'assets/svgs/quran-icon.svg', label: 'Quran'),
+          _bottomBarItem(icon: 'assets/svgs/lamp-icon.svg', label: 'Tips'),
+          _bottomBarItem(icon: 'assets/svgs/pray-icon.svg', label: 'Prayer'),
+          _bottomBarItem(icon: 'assets/svgs/doa-icon.svg', label: 'Doa'),
+          _bottomBarItem(
+              icon: 'assets/svgs/bookmark-icon.svg', label: 'Bookmark'),
+        ],
+        backgroundColor: gray,
+      );
+
+  BottomNavigationBarItem _bottomBarItem(
+          {required String icon, required String label}) =>
+      BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            icon,
+            colorFilter: ColorFilter.mode(text, BlendMode.srcIn),
+          ),
+          activeIcon: SvgPicture.asset(icon,
+              colorFilter: ColorFilter.mode(primary, BlendMode.srcIn)),
+          label: label);
+
+  AppBar _appBar() {
+    return AppBar(
+      backgroundColor: background,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      title: Row(
+        children: [
+          IconButton(
+              onPressed: (() => {}),
+              icon: SvgPicture.asset('assets/svgs/menu-icon.svg')),
+          SizedBox(
+            width: 24,
+          ),
+          Text('Quran App',
+              style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+          const Spacer(
+            flex: 1,
+          ),
+          IconButton(
+              onPressed: (() => {}),
+              icon: SvgPicture.asset('assets/svgs/search-icon.svg')),
+        ],
+      ),
     );
   }
 }
